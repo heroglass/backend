1bb02b560e03e0c1700e73e4bbc125f295e030fa
jun <ggprgrkjh@naver.com>
2024-05-10 01:35:16 +0900

    first init

---
+++ src/main/java/com/junhyeong/heroglass/HeroglassApplication.java
@@ -0,0 +1,13 @@
+package com.junhyeong.heroglass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroglassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroglassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junhyeong/heroglass/HeroglassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junhyeong.heroglass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroglassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
