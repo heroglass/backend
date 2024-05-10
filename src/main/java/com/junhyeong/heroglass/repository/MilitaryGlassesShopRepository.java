0036eba968f0c1a53b7ba0847de9227550e6818b
jun <ggprgrkjh@naver.com>
2024-05-10 22:23:29 +0900

    feat(MilitaryGlassesShopRepository): add MilitaryGlassesShopRepository

---
+++ src/main/java/com/junhyeong/heroglass/repository/MilitaryGlassesShopRepository.java
@@ -0,0 +1,7 @@
+package com.junhyeong.heroglass.repository;
+
+import com.junhyeong.heroglass.domain.MilitaryGlassesShop;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MilitaryGlassesShopRepository extends JpaRepository<MilitaryGlassesShop, Long> {
+}
