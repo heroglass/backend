139419d50c4f6418b02ab56cd6c442fde68ef2b5
jun <ggprgrkjh@naver.com>
2024-05-10 19:56:04 +0900

    feat(JwtAuthenticationFilter): add JwtAuthenticationFilter

---
+++ src/main/java/com/junhyeong/heroglass/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,42 @@
+package com.junhyeong.heroglass.jwt;
+
+
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            jakarta.servlet.FilterChain chain) throws jakarta.servlet.ServletException, IOException {
+
+        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+
+        Authentication authentication = jwtTokenProvider.getAuthentication(token);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        chain.doFilter((jakarta.servlet.ServletRequest) request, (ServletResponse) response);
+    }
+
+
+}
