b7f0b974e89ca10f949ba758765b61a5a9f511c2
jun <ggprgrkjh@naver.com>
2024-05-14 11:58:03 +0900

    feat(Item): add Item entity

--- src/main/java/com/junhyeong/heroglass/domain/item/Item.java
+++ src/main/java/com/junhyeong/heroglass/domain/item/Item.java
@@ -1,9 +1,50 @@
 package com.junhyeong.heroglass.domain.item;
 
+import com.junhyeong.heroglass.exception.NotEnoughStockException;
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToMany;
+import java.util.ArrayList;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속 관계 매핑 -> 한 테이블에 모두 넣는 방식
+@DiscriminatorColumn(name = "dtype")
+@Getter
+@Setter
 public class Item {
-    public void removeStock(int count) {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
     }
 
-    public void addStock(int count) {
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
     }
 }
