766a17d448dde6ee40c07b087dcaef532770ba31
jun <ggprgrkjh@naver.com>
2024-05-18 01:46:29 +0900

    feat(GeocodingService): add GeocodingService

---
+++ src/main/java/com/junhyeong/heroglass/domain/Location.java
@@ -0,0 +1,11 @@
+package com.junhyeong.heroglass.domain;
+
+import jakarta.persistence.Entity;
+
+public class Location {
+    private double latitude;
+    private double longitude;
+
+    public Location(double latitude, double longitude) {
+    }
+}
